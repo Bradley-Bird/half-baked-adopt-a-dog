0029bd64450fc600aed9428030dfe94bad715031
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-09 12:23:06 -0800

    dog cards rendered, and linked to correct id

--- detail/detail.js
+++ detail/detail.js
@@ -4,6 +4,10 @@ import { renderDogDetail } from '../render-utils.js';
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
 // on load
-// get the id from URL
+window.addEventListener('load', async () => {
+    // get the id from URL
+    const params = new URLSearchParams(window.location.search);
+    console.log(params.get('id'));
+});
 // use the id to fetch the dog
 // render and append this dog's details to the container
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,20 +1,22 @@
 const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function getDogs() {
     // from the dogs table, select all items
-
+    const resp = await client.from('dogs').select('*');
+    // console.log(checkError(resp));
     // and return the response (checking for errors)
-    return checkError(response);    
+    return checkError(resp);
 }
 
 export async function getDog(id) {
     // from the dogs table, select a single dog who has the matching id
-
+    const resp = await client.from('dogs').select('*').eq('id', id).single();
     // and return the response (checking for errors)
-    return checkError(response);    
+    return checkError(resp);
 }
 
 function checkError({ data, error }) {
